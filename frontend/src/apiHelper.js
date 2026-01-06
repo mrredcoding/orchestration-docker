aee2e0d24bb3ceabef44d0928e653a6259213dcf
mrredcoding <cedric.alonso.pro@gmail.com>
2026-01-06 16:35:33 +0100

    update api url call to be aws ready

--- frontend/src/apiHelper.js
+++ frontend/src/apiHelper.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost:5000";
+const API_BASE_URL = process.env.API_BASE_URL;
 
 export async function fetchWithAuth(url, options = {}) {
     const token = localStorage.getItem("authToken");
